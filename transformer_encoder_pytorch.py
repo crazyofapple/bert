09f6afffc064687f7ac85d847f082e1c8d1f3ffa
crazyofapple <630450972@qq.com>
2018-11-24 10:58:07 +0800

    create transformer encoder for backup

---
+++ transformer_encoder_pytorch.py
@@ -0,0 +1,271 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import math
+
+
+def _gen_bias_mask(max_length):
+    """
+    Generates bias values (-Inf) to mask future timesteps during attention
+    """
+    np_mask = np.triu(np.full([max_length, max_length], -np.inf), 1)
+    torch_mask = torch.from_numpy(np_mask).type(torch.FloatTensor)
+
+    return torch_mask.unsqueeze(0).unsqueeze(1)
+
+
+def _gen_timing_signal(length, channels, min_timescale=1.0, max_timescale=1.0e4):
+    position = np.arange(length)
+    num_timescales = channels // 2
+    log_timescale_increment = (
+            math.log(float(max_timescale) / float(min_timescale)) /
+            (float(num_timescales) - 1))
+    inv_timescales = min_timescale * np.exp(
+        np.arange(num_timescales).astype(np.float) * -log_timescale_increment)
+    scaled_time = np.expand_dims(position, 1) * np.expand_dims(inv_timescales, 0)
+
+    signal = np.concatenate([np.sin(scaled_time), np.cos(scaled_time)], axis=1)
+    signal = np.pad(signal, [[0, 0], [0, channels % 2]],
+                    'constant', constant_values=[0.0, 0.0])
+    signal = signal.reshape([1, length, channels])
+
+    return torch.from_numpy(signal).type(torch.FloatTensor)
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, input_depth, total_key_depth, total_value_depth, output_depth,
+                 num_heads, bias_mask=None, dropout=0.0):
+
+        super(MultiHeadAttention, self).__init__()
+
+        if total_key_depth % num_heads != 0:
+            raise ValueError("Key depth (%d) must be divisible by the number of "
+                             "attention heads (%d)." % (total_key_depth, num_heads))
+        if total_value_depth % num_heads != 0:
+            raise ValueError("Value depth (%d) must be divisible by the number of "
+                             "attention heads (%d)." % (total_value_depth, num_heads))
+
+        self.num_heads = num_heads
+        self.query_scale = (total_key_depth // num_heads) ** -0.5
+        self.bias_mask = bias_mask
+
+        # Key and query depth will be same
+        self.query_linear = nn.Linear(input_depth, total_key_depth, bias=False)
+        self.key_linear = nn.Linear(input_depth, total_key_depth, bias=False)
+        self.value_linear = nn.Linear(input_depth, total_value_depth, bias=False)
+        self.output_linear = nn.Linear(total_value_depth, output_depth, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def _split_heads(self, x):
+
+        if len(x.shape) != 3:
+            raise ValueError("x must have rank 3")
+        shape = x.shape
+        return x.view(shape[0], shape[1], self.num_heads, shape[2] // self.num_heads).permute(0, 2, 1, 3)
+
+    def _merge_heads(self, x):
+
+        if len(x.shape) != 4:
+            raise ValueError("x must have rank 4")
+        shape = x.shape
+        return x.permute(0, 2, 1, 3).contiguous().view(shape[0], shape[2], shape[3] * self.num_heads)
+
+    def forward(self, queries, keys, values):
+
+        # Do a linear for each component
+        queries = self.query_linear(queries)
+        keys = self.key_linear(keys)
+        values = self.value_linear(values)
+
+        # Split into multiple heads
+        queries = self._split_heads(queries)
+        keys = self._split_heads(keys)
+        values = self._split_heads(values)
+
+        # Scale queries
+        queries *= self.query_scale
+
+        # Combine queries and keys
+        logits = torch.matmul(queries, keys.permute(0, 1, 3, 2))
+
+        # Add bias to mask future values
+        if self.bias_mask is not None:
+            logits += self.bias_mask[:, :, :logits.shape[-2], :logits.shape[-1]].type_as(logits.data)
+
+        # Convert to probabilites
+        weights = nn.functional.softmax(logits, dim=-1)
+
+        # Dropout
+        weights = self.dropout(weights)
+
+        # Combine with values to get context
+        contexts = torch.matmul(weights, values)
+
+        # Merge heads
+        contexts = self._merge_heads(contexts)
+        # contexts = torch.tanh(contexts)
+
+        # Linear to get output
+        outputs = self.output_linear(contexts)
+
+        return outputs
+
+
+class PositionwiseFeedForward(nn.Module):
+    """
+    Does a Linear + RELU + Linear on each of the timesteps
+    """
+
+    def __init__(self, input_depth, filter_size, output_depth, layer_config='ll', padding='left', dropout=0.0):
+
+        super(PositionwiseFeedForward, self).__init__()
+
+        layers = []
+        sizes = ([(input_depth, filter_size)] +
+                 [(filter_size, filter_size)] * (len(layer_config) - 2) +
+                 [(filter_size, output_depth)])
+
+        for lc, s in zip(list(layer_config), sizes):
+            if lc == 'l':
+                layers.append(nn.Linear(*s))
+            elif lc == 'c':
+                layers.append(Conv(*s, kernel_size=3, pad_type=padding))
+            else:
+                raise ValueError("Unknown layer type {}".format(lc))
+
+        self.layers = nn.ModuleList(layers)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, inputs):
+        x = inputs
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            if i < len(self.layers):
+                x = self.relu(x)
+                x = self.dropout(x)
+
+        return x
+
+
+class Conv(nn.Module):
+
+    def __init__(self, input_size, output_size, kernel_size, pad_type):
+        super(Conv, self).__init__()
+        padding = (kernel_size - 1, 0) if pad_type == 'left' else (kernel_size // 2, (kernel_size - 1) // 2)
+        self.pad = nn.ConstantPad1d(padding, 0)
+        self.conv = nn.Conv1d(input_size, output_size, kernel_size=kernel_size, padding=0)
+
+    def forward(self, inputs):
+        inputs = self.pad(inputs.permute(0, 2, 1))
+        outputs = self.conv(inputs).permute(0, 2, 1)
+
+        return outputs
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(features))
+        self.beta = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.gamma * (x - mean) / (std + self.eps) + self.beta
+
+
+class EncoderLayer(nn.Module):
+
+    def __init__(self, hidden_size, total_key_depth, total_value_depth, filter_size, num_heads,
+                 bias_mask=None, layer_dropout=0.0, attention_dropout=0.0, relu_dropout=0.0):
+        super(EncoderLayer, self).__init__()
+
+        self.multi_head_attention = MultiHeadAttention(hidden_size, total_key_depth, total_value_depth,
+                                                       hidden_size, num_heads, bias_mask, attention_dropout)
+
+        self.positionwise_feed_forward = PositionwiseFeedForward(hidden_size, filter_size, hidden_size,
+                                                                 layer_config='cc', padding='both',
+                                                                 dropout=relu_dropout)
+        self.dropout = nn.Dropout(layer_dropout)
+        self.layer_norm_mha = LayerNorm(hidden_size)
+        self.layer_norm_ffn = LayerNorm(hidden_size)
+
+    def forward(self, inputs):
+        x = inputs
+
+        # Layer Normalization
+        x_norm = self.layer_norm_mha(x)
+
+        # Multi-head attention
+        y = self.multi_head_attention(x_norm, x_norm, x_norm)
+
+        # Dropout and residual
+        x = self.dropout(x + y)
+
+        # Layer Normalization
+        x_norm = self.layer_norm_ffn(x)
+
+        # Positionwise Feedforward
+        y = self.positionwise_feed_forward(x_norm)
+
+        # Dropout and residual
+        y = self.dropout(x + y)
+
+        return y
+
+
+class Encoder(nn.Module):
+    def __init__(self, embedding_size, hidden_size, num_layers, num_heads, total_key_depth, total_value_depth,
+                 filter_size, max_length=100, input_dropout=0.0, layer_dropout=0.0,
+                 attention_dropout=0.0, relu_dropout=0.0, use_mask=False):
+        super(Encoder, self).__init__()
+
+        self.timing_signal = _gen_timing_signal(max_length, hidden_size)
+
+        params = (hidden_size,
+                  total_key_depth or hidden_size,
+                  total_value_depth or hidden_size,
+                  filter_size,
+                  num_heads,
+                  _gen_bias_mask(max_length) if use_mask else None,
+                  layer_dropout,
+                  attention_dropout,
+                  relu_dropout)
+
+        self.embedding_proj = nn.Linear(embedding_size, hidden_size, bias=False)
+        self.enc = nn.Sequential(*[EncoderLayer(*params) for l in range(num_layers)])
+
+        self.layer_norm = LayerNorm(hidden_size)
+        self.input_dropout = nn.Dropout(input_dropout)
+
+    def forward(self, inputs):
+        # Add input dropout
+
+        x = self.input_dropout(inputs)
+
+        # Project to hidden size
+        x = self.embedding_proj(x)
+
+        # Add timing signal
+        x += self.timing_signal[:, :inputs.shape[1], :].type_as(inputs.data)
+
+        y = self.enc(x)
+
+        y = self.layer_norm(y)
+        return y
+
+
+check_transformer = Encoder(384, 384, 1, 1, 384, 384, 64, max_length=2000, input_dropout=0.0, layer_dropout=0.0,
+                            attention_dropout=0.0, relu_dropout=0.0, use_mask=False)
+
+check_transformer.cuda()
